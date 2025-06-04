701307dfac15a4fb3d0ed3359eca9fe7f4161335
uddhave <bhanuseuddhav85@gmail.com>
2025-06-04 12:59:04 +0530

    add new

--- models.py
+++ models.py
@@ -1,4 +1,3 @@
-
 from sqlalchemy import Column, Integer, String
 from database import Base
 
@@ -6,6 +5,6 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String(50), unique=True, index=True)
-    email = Column(String(100), unique=True, index=True)
-    hashed_password = Column(String(255))
+    username = Column(String(50), unique=True, index=True, nullable=False)
+    email = Column(String(100), unique=True, index=True, nullable=False)
+    hashed_password = Column(String(128), nullable=False)
